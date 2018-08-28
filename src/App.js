565efcd0590135c91468843baadaa57cfe1f4bf5
RyanHsu <mvpdw06@gmail.com>
2018-08-29 01:51:45 +0800

    Test react-decoration HOC of react component function - failed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import Enhancer from './Enhancer'
+import { throttle } from 'react-decoration'
 
 @Enhancer
 class App extends Component {
+  @throttle(2000)
+  pushButton() {
+    console.log('I am pushed.')
+  }
   render() {
-    return <div>Hey You!</div>
+    return (
+    <>
+      Hey You!
+      <button onClick={this.pushButton}>Push me!!!!</button>
+    </>
+    )
   }
 }
 
